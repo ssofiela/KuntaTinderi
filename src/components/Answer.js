f1a2d9850ef2140a1290939d4e9d3d76d87b5b60
Tomi <tomipanula1@gmail.com>
2019-10-12 17:35:42 +0300

    question charts

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,43 +1,58 @@
-import React from 'react'
-import {View, Button, Text} from 'react-native'
-import {connect} from 'react-redux'
-import {addMessage, deleteMessage} from '../store/actions/actions'
-import { Ionicons } from '@expo/vector-icons';
-import colors from '../common/colors'
+import React from "react";
+import { View, Button, Text } from "react-native";
+import { connect } from "react-redux";
+import { addMessage, deleteMessage } from "../store/actions/actions";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../common/colors";
 
 class Answer extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render(){
-        return(
-            <View style={{display:"flex", flexDirection:"row", backgroundColor: colors.DARK_GRAY}}>
-                <Text style={{color:colors.YELLOW}}s>{this.props.title}</Text>
-                {
-                    <Ionicons name={this.props.answer ? "md-checkmark-circle" : "md-close-circle"} size={32} color={colors.YELLOW}/>
-                }
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          backgroundColor: colors.DARK_GRAY,
+
+          alignItems: "center",
+          justifyContent: "space-between"
+        }}
+      >
+        <Text
+          style={{ color: colors.YELLOW, marginHorizontal: 10, fontSize: 20 }}
+        >
+          {this.props.title}
+        </Text>
+        <Ionicons
+          name={this.props.answer ? "md-checkmark-circle" : "md-close-circle"}
+          size={48}
+          color={colors.YELLOW}
+          style={{ marginRight: 40, marginTop: 15, marginBottom: 15 }}
+        />
+      </View>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-      messages: state.messages
-    }
-  }
-  
-  const mapDispatchToProps = dispatch => {
-      return{
+  return {
+    messages: state.messages
+  };
+};
 
-          addMessage: () => dispatch(addMessage()),
-          deleteMessage: () => dispatch(deleteMessage())
+const mapDispatchToProps = dispatch => {
+  return {
+    addMessage: () => dispatch(addMessage()),
+    deleteMessage: () => dispatch(deleteMessage())
+  };
+};
 
-        }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Answer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Answer);
---
+++ src/components/QuestionData.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { View, Button, Text } from "react-native";
+import { connect } from "react-redux";
+import { addMessage, deleteMessage } from "../store/actions/actions";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../common/colors";
+
+import { BarChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
+
+const ageGroups = ["10-19", "20-29", "30-39", "40-49", "50-59", "60-69"];
+
+class QuestionData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View
+        style={{
+          height: 200,
+          display: "flex",
+          flexDirection: "row",
+          marginHorizontal: 20,
+          marginTop: 10
+        }}
+      >
+        <YAxis
+          data={this.props.data}
+          style={{}}
+          contentInset={{ top: 10, bottom: 25 }}
+          svg={{
+            fill: colors.YELLOW,
+            fontSize: 10
+          }}
+          formatLabel={value => `${value}%`}
+          min={0}
+        />
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <BarChart
+            style={{ flex: 1 }}
+            data={this.props.data}
+            svg={{ fill: colors.YELLOW }}
+            yMin={0}
+            contentInset={{ top: 10, bottom: 10 }}
+          >
+            <Grid svg={{ stroke: colors.YELLOW, strokeOpacity: 0.5 }} />
+          </BarChart>
+          <XAxis
+            data={this.props.data}
+            formatLabel={(value, index) => ageGroups[index]}
+            contentInset={{ left: 30, right: 30 }}
+            svg={{ fontSize: 10, fill: colors.YELLOW }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    messages: state.messages
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addMessage: () => dispatch(addMessage()),
+    deleteMessage: () => dispatch(deleteMessage())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(QuestionData);
